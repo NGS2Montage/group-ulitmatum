1a356ac888d9cf1087363f6a84ec333137e03a77
Nathan Self <nwself@cs.vt.edu>
2017-01-18 14:05:32 -0500

    Got first incomplete example of state checking middleware working

--- core/middleware.py
+++ core/middleware.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect
 
 from .models import Game
 
+
 class GameStateRedirectMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
@@ -12,12 +13,22 @@ class GameStateRedirectMiddleware(object):
         # the current game's state. Redirect to error pages if they are
         # too early or too late.
 
+        print(request.user)
+
+        # if request.user.is_anonymous():
+        #     # How do we get here?
+        #     redirect('state-mismatch')
+
+        print(request.user.userstate)
         g = Game.objects.get_current_game()
-        
-        if g.state == 'g1' and request.user.userstate.state == 's1':
+
+        if g.state == 'g1' and request.user.userstate.state == 's1' and 'state-mismatch' not in request.path:
             # game already started but user hasn't completed survey 1
+            print("not asking for state-mismatch but state mismatched")
             return redirect('state-mismatch')
 
+
+        print("either correctly asking for state-mismatch or states match")
         # Else: this request is fine, pass it along
         response = self.get_response(request)
 
@@ -26,3 +37,8 @@ class GameStateRedirectMiddleware(object):
         # any for this middleware.
 
         return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        print("processing view")
+
+        return None
--- ngs2/settings.py
+++ ngs2/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'core.middleware.GameStateRedirectMiddleware',
+    'core.middleware.GameStateRedirectMiddleware',
 ]
 
 if DEBUG:
