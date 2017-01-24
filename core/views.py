5a1e74dcdfec856dab1418eca70588679bef06ff
Nathan Self <nwself@cs.vt.edu>
2017-01-24 15:24:55 -0500

    Add state-mismatch template

--- core/views.py
+++ core/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth.decorators import login_required
 
 from annoying.decorators import render_to
 
+from .models import Game
+
 
 @render_to('state-mismatch.html')
 @login_required
 def state_mismatch(request):
-    context = {}
+    context = {
+        "game": Game.objects.get_current_game()
+    }
     # fill in context dict with stuff to pass to template as needed
     return context
