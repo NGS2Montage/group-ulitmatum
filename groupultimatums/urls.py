bf81540a6f39783842d4090733265ec0b8e4060a
Nathan Self <nwself@cs.vt.edu>
2017-01-30 14:28:00 -0500

    Rename urls to hide game title

--- groupultimatums/urls.py
+++ groupultimatums/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^survey/', views.survey, name="group-ultimatums-survey"),
     url(r'^tutorial/', views.tutorial, name="group-ultimatums-tutorial"),
     url(r'^waiting-room/', views.waiting_room, name="group-ultimatums-waiting"),
-    url(r'^group-ultimatum-part-1/', views.game_initial, name="group-ultimatums-game-1"),
-    url(r'^group-ultimatum-part-2/', views.game_voting, name="group-ultimatums-game-2"),
-    url(r'^group-ultimatum-part-3/', views.game_final, name="group-ultimatums-game-3"),
+    url(r'^part1/', views.game_initial, name="group-ultimatums-game-1"),
+    url(r'^part2/', views.game_voting, name="group-ultimatums-game-2"),
+    url(r'^part3/', views.game_final, name="group-ultimatums-game-3"),
 ]
--- publicgoods/urls.py
+++ publicgoods/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^survey/', views.survey, name="public-goods-survey"),
     url(r'^tutorial/', views.tutorial, name="public-goods-tutorial"),
     url(r'^waiting-room/', views.waiting_room, name="public-goods-waiting"),
-    url(r'^public-goods/', views.game, name="public-goods-game"),
+    url(r'^part1/', views.game, name="public-goods-game"),
 ]
