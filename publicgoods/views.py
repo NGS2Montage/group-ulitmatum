5575c5cae3a4a8471f7f2769aff37cd830ff2e82
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-02-13 10:13:13 -0500

    added rough starting code to phase 2

--- publicgoods/views.py
+++ publicgoods/views.py
@@ -4,6 +4,7 @@ from annoying.decorators import render_to
 
 from core.decorators import game_state_required
 
+import re
 
 valid_state_redirects = {
     "w2": {
@@ -18,7 +19,40 @@ valid_state_redirects = {
 @render_to('publicgoods/game.html')
 @login_required
 def game(request):
+    
+    def is_valid_number(s):
+        f = 0
+        try:
+            f = float(s)
+            if not re.match(r'^[0-9]*\.[0-9]{2}$', s):
+                return "the amount format is invalid"
+            else:
+                return ""  
+        except ValueError:
+            return "the dollar amount is not a number"
+
+
     context = {}
+    context["error"] = ""
+
+    if (request.method == 'POST'):
+        
+        d_amt = request.POST["dollar_amt"]
+
+        # validate the dollar amount
+        error = is_valid_number(d_amt)
+        result = 0
+        if (len(error) == 0):
+            d_amt = float(d_amt)
+            result = d_amt
+        else:
+            result = 0
+            context["error"] = error
+
+        context["d_amt"] = result
+    else:
+        context["d_amt"] = 0
+
     # fill in context dict with stuff to pass to template as needed
     return context
 
@@ -27,6 +61,7 @@ def game(request):
 @render_to('publicgoods/waiting.html')
 @login_required
 def waiting_room(request):
+    
     context = {}
     # fill in context dict with stuff to pass to template as needed
     return context
